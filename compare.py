f96db67c786a3f09fa39ddd91598fcb9bbb53581
Rachel4git <780169863@qq.com>
2017-12-25 19:58:37 +0800

    bagdata committed

--- compare.py
+++ compare.py
@@ -40,28 +40,30 @@ def checking(dict1,dict2):  ###dict1为数据库，dict2为文件
                 subbaggae = bag_db.get_subbag('10.100.157.78', 3500, 'TCFlyIntFare', 'MDiNkMR85fKgyRXI3iR', 'TCFlyIntFare',
                                'SELECT * FROM TCFlyIntFare.fifc_fsd_sub_baggage where relation_id=%s', dict1["id"])
                 #c=1111
-                dd=subbaggae[0]
-                subbaggae=dict1.setdefault(subbaggae[0]["id"],subbaggae[0])
-                compare(subbaggae["bags"], dict2["bags"], "bags错误")
+                # dd=subbaggae[0]
+                subbaggae=dict1.setdefault("subbaggae",subbaggae[0])
+                # ff=subbaggae["bags"]
+                # gg=dict2["bags"]
+                compare(subbaggae["bags"], dict2["bags"], "8:bags错误")
             else:
                 if dict2[id]["sub_tag"]==0 and dict1[id]["sub_tag"]==0:
-                    compare(dict1["bags"], dict2["bags"], "bags错误")
+                    compare(dict1["bags"], dict2["bags"], "8:bags错误")
                 else:
-                    compare(dict1["sub_tag"], dict2["sub_tag"], "sub_tag错误")
+                    compare(dict1["sub_tag"], dict2["sub_tag"], "9:sub_tag错误")
             #对比其他字段
             # d=dict1["routing"]
             # e=dict2["routing"]
-            F=compare(dict1["routing"],dict2["routing"],"routing错误")
-            compare(dict1["connections"],dict2["connections"],"connections错误")
-            compare(dict1["fillingairline"], dict2["fillingairline"], "fillingairline错误")
-            compare(dict1["aircodes"], dict2["aircodes"], "aircodes错误")
-            compare(dict1["cabinclass"],dict2["cabinclass"],"cabinclass错误")
-            compare(dict1["pax_type"], dict2["pax_type"],"pax_Type错误")
-            compare(dict1["create_time"], dict2["create_time"],"createTime错误")
-            compare(dict1["source"],dict2["source"],"source错误")
-            rr=compare(dict1["tracerid"], dict2["tracerid"],"tracerId错误")
-            G=compare(dict1["dep_city"], dict2["dep_city"], "dep_city错误")
-            h=compare(dict1["arr_city"], dict2["arr_city"], "dep_city错误")
+            F=compare(dict1["routing"],dict2["routing"],"2:routing错误")
+            compare(dict1["connections"],dict2["connections"],"3:connections错误")
+            compare(dict1["fillingairline"], dict2["fillingairline"], "4:fillingairline错误")
+            compare(dict1["aircodes"], dict2["aircodes"], "5:aircodes错误")
+            compare(dict1["cabinclass"],dict2["cabinclass"],"6:cabinclass错误")
+            compare(dict1["pax_type"], dict2["pax_type"],"7:pax_Type错误")
+            compare(dict1["create_time"], dict2["create_time"],"10:createTime错误")
+            compare(dict1["source"],dict2["source"],"11:source错误")
+            rr=compare(dict1["tracerid"], dict2["tracerid"],"12:tracerId错误")
+            G=compare(dict1["dep_city"], dict2["dep_city"], "13:dep_city错误")
+            h=compare(dict1["arr_city"], dict2["arr_city"], "14:arr_city错误")
             compare(dict1["detail_link"], dict2["detail_link"],"detail_link错误")
 
             # self.cmp(dict1[id]["bags"], dict2[id]["bags"], "bags错误")
@@ -85,7 +87,7 @@ if __name__ == '__main__':
             "cabinclass"]
      dict1 = db_dict[json_dict["routing"] +"|"+ json_dict["connections"] + "|"+json_dict["fillingairline"] + "|"+json_dict["cabinclass"]] #数据库
      result=checking(dict1, dict2)
-     print ("ERROR LIST")
+     print ("ERROR LIST :---------------------")
      # for iin range(0,len(error_list)):
      print (error_list) ####？？？？？？？？？？？为什么一条错误信息存入多次？？？？？？？？？？？？？？？？
     except Exception,E:  #异常信息有时是因为key与数据库中的字段值不匹配
