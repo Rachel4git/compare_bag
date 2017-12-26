54b94a8259d2fe7f4299299e539af751fc1ad42b
Rachel4git <780169863@qq.com>
2017-12-26 20:13:45 +0800

    bagdata committed

--- compare.py
+++ compare.py
@@ -46,25 +46,25 @@ def checking(dict1,dict2):  ###dict1为数据库，dict2为文件
                 # gg=dict2["bags"]
                 compare(subbaggae["bags"], dict2["bags"], "8:bags错误")
             else:
-                if dict2[id]["sub_tag"]==0 and dict1[id]["sub_tag"]==0:
+                if dict2["sub_tag"]==0 and dict1["sub_tag"]==0:
                     compare(dict1["bags"], dict2["bags"], "8:bags错误")
                 else:
                     compare(dict1["sub_tag"], dict2["sub_tag"], "9:sub_tag错误")
             #对比其他字段
             # d=dict1["routing"]
             # e=dict2["routing"]
-            F=compare(dict1["routing"],dict2["routing"],"2:routing错误")
-            compare(dict1["connections"],dict2["connections"],"3:connections错误")
-            compare(dict1["fillingairline"], dict2["fillingairline"], "4:fillingairline错误")
-            compare(dict1["aircodes"], dict2["aircodes"], "5:aircodes错误")
-            compare(dict1["cabinclass"],dict2["cabinclass"],"6:cabinclass错误")
-            compare(dict1["pax_type"], dict2["pax_type"],"7:pax_Type错误")
-            compare(dict1["create_time"], dict2["create_time"],"10:createTime错误")
-            compare(dict1["source"],dict2["source"],"11:source错误")
-            rr=compare(dict1["tracerid"], dict2["tracerid"],"12:tracerId错误")
-            G=compare(dict1["dep_city"], dict2["dep_city"], "13:dep_city错误")
-            h=compare(dict1["arr_city"], dict2["arr_city"], "14:arr_city错误")
-            compare(dict1["detail_link"], dict2["detail_link"],"detail_link错误")
+        F=compare(dict1["routing"],dict2["routing"],"2:routing错误")
+        compare(dict1["connections"],dict2["connections"],"3:connections错误")
+        compare(dict1["fillingairline"], dict2["fillingairline"], "4:fillingairline错误")
+        compare(dict1["aircodes"], dict2["aircodes"], "5:aircodes错误")
+        compare(dict1["cabinclass"],dict2["cabinclass"],"6:cabinclass错误")
+        compare(dict1["pax_type"], dict2["pax_type"],"7:pax_Type错误")
+        compare(dict1["create_time"], dict2["create_time"],"10:createTime错误")
+        compare(dict1["source"],dict2["source"],"11:source错误")
+        rr=compare(dict1["tracerid"], dict2["tracerid"],"12:tracerId错误")
+        G=compare(dict1["dep_city"], dict2["dep_city"], "13:dep_city错误")
+        h=compare(dict1["arr_city"], dict2["arr_city"], "14:arr_city错误")
+        compare(dict1["detail_link"], dict2["detail_link"],"detail_link错误")
 
             # self.cmp(dict1[id]["bags"], dict2[id]["bags"], "bags错误")
             # self.cmp(dict1[id]["sub_tag"],dict2[id]["sub_tag"], "sub_tag错误")
@@ -77,21 +77,21 @@ if __name__ == '__main__':
     db_dict = bag_db.db2dict(db)
 
     # 读入文件
-    json = bag_json.json_read(r'D:\MyConfiguration\hd48552\Desktop\sss1.txt')
+    json = bag_json.json_read(r'D:\MyConfiguration\hd48552\Desktop\ssss.txt')
     json_dict = bag_json.proProcess(json)
 
     dict2 = json_dict #文件
     # a=json_dict["routing"] + "|" + json_dict["connections"] + "|" + json_dict["fillingairline"] + "|" + json_dict["cabinclass"]
-    try:
-     a=json_dict["routing"] + "|" + json_dict["connections"] + "|" + json_dict["fillingairline"] + "|" + json_dict[
+    # try:
+    a=json_dict["routing"] + "|" + json_dict["connections"] + "|" + json_dict["fillingairline"] + "|" + json_dict[
             "cabinclass"]
-     dict1 = db_dict[json_dict["routing"] +"|"+ json_dict["connections"] + "|"+json_dict["fillingairline"] + "|"+json_dict["cabinclass"]] #数据库
-     result=checking(dict1, dict2)
-     print ("ERROR LIST :---------------------")
+    dict1 = db_dict[json_dict["routing"] +"|"+ json_dict["connections"] + "|"+json_dict["fillingairline"] + "|"+json_dict["cabinclass"]] #数据库
+    result=checking(dict1, dict2)
+    print ("ERROR LIST :---------------------")
      # for iin range(0,len(error_list)):
-     print (error_list) ####？？？？？？？？？？？为什么一条错误信息存入多次？？？？？？？？？？？？？？？？
-    except Exception,E:  #异常信息有时是因为key与数据库中的字段值不匹配
-         print Exception, ":", E#"111111111116666666666666666"
+    print (error_list) ####？？？？？？？？？？？为什么一条错误信息存入多次？？？？？？？？？？？？？？？？
+    # except Exception,E:  #异常信息有时是因为key与数据库中的字段值不匹配
+    # print Exception, ":", E#"111111111116666666666666666"
 
 
 
